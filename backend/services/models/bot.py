258d606bbb775e3fae189a69cd6bb3612911ae7a
Huleji Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-10-18 10:40:19 -0500

    Create bot.py

---
+++ backend/services/models/bot.py
@@ -0,0 +1,27 @@
+# backend/models/bot.py
+
+from . import db
+
+class Bot(db.Model):
+    __tablename__ = 'bots'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    stats = db.Column(db.PickleType, nullable=False)
+    wins = db.Column(db.Integer, default=0)
+    losses = db.Column(db.Integer, default=0)
+    experience = db.Column(db.Integer, default=0)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'stats': self.stats,
+            'wins': self.wins,
+            'losses': self.losses,
+            'experience': self.experience,
+            'user_id': self.user_id,
+            'owner_username': self.owner.username
+        }
